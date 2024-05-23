f35f55a19d610c273f57e9389c2dcb6e11465f88
Teteh <124629621+StephanyAndreina@users.noreply.github.com>
2024-05-23 09:30:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app = Flask(__name__)
 @app.route('/')
 def devops():
  return '<center><h1><font color=red>Germinare Tech, EU AMO DEVOPS</center>'
+
+def salah():
+ return '<center><h1><font color=red>AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</center>'
 if __name__ == '__main__':
  app.run(debug=True, host='0.0.0.0')
